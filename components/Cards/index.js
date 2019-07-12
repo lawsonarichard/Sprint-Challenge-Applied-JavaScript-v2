17a2e1f7789937892435190282f13f48b2075dd6
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-12 09:34:05 -0700

    completed header moving on to cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,34 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cards = document.querySelector('cards-container');
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(data => {
+    console.log('the data is here', data)
+    const cardData = cardInfo(data.data)
+    cards.appendChild(cardData);
+    
+})
+.catch(err => {
+    console.log('data is messed up')
+});
+function cardComponent(cardObj){
+    const card = document.createElement('div');
+    const headline = doucment.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const by = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+    by.classList.add('span');
+
+    image.src = imgUrl;
+
+    card.append(headline, author, imgContainer, by);
+    imgContainer.appendChild(image);
+    
+    return cards
+}
